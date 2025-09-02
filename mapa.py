7a9351f18d64ff5097dca4972b5d6e34d831a9c9
fcosolito <cosolitofranco@gmail.com>
2025-09-02 13:58:33 -0300

    Borrar archivo viejo

--- mapa.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-mapa = gpd.read_file("ProvinciasArgentina.geojson")
-
-# Colorear provincias con colores aleatorios
-mapa.plot(column=None, cmap='tab20', edgecolor='black')
-
-plt.show()
